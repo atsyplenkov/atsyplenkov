26e9e4fe00926927d10c7b30c6dbdba3a34163d5
Anatolii Tsyplenkov <34775595+atsyplenkov@users.noreply.github.com>
2025-04-27 00:55:47 +0000

    Update update_citations.py

--- .github/workflows/update_citations.py
+++ .github/workflows/update_citations.py
@@ -5,10 +5,12 @@ scholar_id = "IcwW-WAAAAAJ"
 
 # Fetch author profile
 author = scholarly.search_author_id(scholar_id)
+print("Author found")
 author = scholarly.fill(author, sections=["indices"])
 
 # Get total citations
 citations = author["citedby"]
+print("Citation parsed")
 
 # Write to file
 with open("citations.json", "w") as f:
